442627d2908ca47a3722996ef5917a73f7e9ceb1
sfj.eth <0x0xsfj@gmail.com>
2021-10-27 23:53:59 -0700

    Chakra UI / Mint Button

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  // fonts: {
+  //   heading: 'Syne',
+  //   body: 'Syne',
+  // },
+  colors: {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#FAE9D1',
+    whiteAlpha: {
+      900: '#FAE9D1',
+    },
+    gray: {
+      50: 'green',
+      700: 'purple',
+      800: '#000',
+      900: 'orange',
+    },
+  },
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+});
+
+// const theme = extendTheme({ colors, config })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-
-export default MyApp
+export default MyApp;
